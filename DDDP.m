a9f0e46af11cb1dca47e9ea651c8e8d4875c2715
bottom0 <80024076+bottom0@users.noreply.github.com>
2023-06-25 16:36:43 +0800

    Update DDDP.m

--- DDDP.m
+++ DDDP.m
@@ -18,13 +18,13 @@ clc,clear
 % Hsl            耗水率（发电流量/水量）
 
 %% 初始化数据
-KSJ=xlsread('result_Q——1.xlsx'); %发电流量即初始可行解
+KSJ=xlsread('cgb(1).xlsx'); %发电流量即初始可行解
 Q=KSJ(:,3);
-max_E_eco_=KSJ(1,5);
-QS=398.49; % 起始水位
-ZZ=397.72; % 终止水位
-C=1; % 增量
-BC=0.01; % 步长
+max_E_eco_=KSJ(22,4);
+QS=397.62; % 起始水位
+ZZ=397.415305592157; % 终止水位
+C=10; % 增量
+BC=1; % 步长
 
 %导入入库流量和水库库容与水位关系
 filename1= "试验数据1.txt";
@@ -32,15 +32,24 @@ delimiterIn1=" ";
 headerlinesIn1=4;
 test1=importdata(filename1,delimiterIn1,headerlinesIn1);
 Q_IN=test1.data;% 入库流量
+filename2= "试验数据2.txt";
+delimiterIn2=" ";
+headerlinesIn2=7;
+test2=importdata(filename2,delimiterIn2,headerlinesIn2);
+dateset2=test2.data; %调度图
+headerlinesIn2_1=33;
+test2_1=importdata(filename2,delimiterIn2,headerlinesIn2_1);
+CL=test2_1.data; %指示出力
 filename3= "试验数据3.txt";
 delimiterIn3=" ";
 headerlinesIn3=5;
 test3=importdata(filename3,delimiterIn3,headerlinesIn3);
 dateset3=test3.data; %水库库容 
-V_max=quest_v_Z_V(412,dateset3)*ones(20,1); %最大库容
+
+V_max=quest_v_Z_V(413,dateset3)*ones(20,1); %最大库容
 V_min=quest_v_Z_V(380,dateset3)*ones(20,1); %最小库容
-V_SYT=quest_v_Z_V(QS,dateset3); %时段末库容
-V_SYT0 = quest_v_Z_V(ZZ,dateset3); %时段初库容
+V_SYT=quest_v_Z_V(ZZ,dateset3); %时段末库容
+V_SYT0 = quest_v_Z_V(QS,dateset3); %时段初库容
 for i=1:20
 Q_fd(:,i)=Q(i)-C:BC:Q(i)+C; % Q_fd 发电流量可能取值
 end
@@ -60,13 +69,14 @@ Q_fd_opt=zeros(a,T);
 
 %% 先计算第一次
 % 从最后一天开始逆向递推
+
 V_opt(1,T+1) = V_SYT; 
 V_opt_num(1,T+1) = string(V_SYT);
 E_eco_(1,T+1) = 0;% 最后一天时段末最优出力为0
 E_eco_stage(1,T+1) = 0;
 Hsl(1,T+1)=0;
 
-while 1
+while 1 
 for i=1:20
 Q_fd(:,i)=Q(i)-C:BC:Q(i)+C; % Q_fd 发电流量可能取值
 end
@@ -74,7 +84,7 @@ end
     [V_opt, V_opt_num, E_eco_, Q_fd_opt, Q_qs,E_eco_stage, Hsl] = water_reservoir_optim(V_max, V_min,V_SYT0,...
                                                                V_opt, V_opt_num, E_eco_,...
                                                                 Q_fd_opt, Q_qs, Q_fd,Q_IN,...
-                                                               t,tim(t),a, dateset3,E_eco_stage, Hsl);
+                                                               t,tim(t),a, CL, dateset2, dateset3,E_eco_stage, Hsl);
     end
 
 
@@ -89,18 +99,13 @@ end
         ANWER_E_eco_(an) = E_eco_(r,an);
         ANWER_Hsl(an) = Hsl(r,an);
     end
-% if sqrt(sum((ANWER_Q_fd-Q).*(ANWER_Q_fd-Q)))<1
-%     break
-% else
-%     Q=ANWER_Q_fd;
-% end
-if (abs(ANWER_E_eco_(1)-max_E_eco_)/max_E_eco_)<0.000001
+if (abs(ANWER_E_eco_(1)-max_E_eco_)/max_E_eco_)<0.0000001%0.000001
     break
 else
     Q=ANWER_Q_fd;
-    max_E_eco_=ANWER_E_eco_(1)
+    max_E_eco_=ANWER_E_eco_(1);
 end
 end
 %%
-xlswrite('result_Q——6.xlsx',[{'水位'},{'库容'},{'发电流量'},{'发电量'},{'总发电量'},{'耗水率'}],1,'A1');
-xlswrite('result_Q——6.xlsx',[ANWER_Z',ANWER_V(1:20)',ANWER_Q_fd', ANWER_E_eco_stage',ANWER_E_eco_',ANWER_Hsl'],1,'A2');     
+xlswrite('cgb(1)_lyl.xlsx',[{'水位'},{'库容'},{'发电流量'},{'发电量'},{'总发电量'},{'耗水率'}],1,'A1');
+xlswrite('cgb(1)_lyl.xlsx',[ANWER_Z',ANWER_V(1:20)',ANWER_Q_fd', ANWER_E_eco_stage',ANWER_E_eco_',ANWER_Hsl'],1,'A2');     
